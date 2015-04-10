31088742bb89db7aae014e4355a2327406160e14
Ashley Baldwin-Hunter <ashley.baldwinhunter@gmail.com>
2015-04-10 18:57:19 -0400

    adds methods for accessing row, column and board indices

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -7,7 +7,18 @@ class Sudoku
     @board = board_string.split("")
   end
 
+  def rows
+    (0..80).each_slice(9).map {|a| a}
+  end
 
+  def cols
+    rows.transpose
+  end
+
+  def blocks
+    block_corners = [0, 3, 6, 27, 30, 33, 54, 57, 60]
+    block_corners.map {|i| [i, i+1, i+2, i+9, i+10, i+11, i+18, i+19, i+20]}
+  end
 
   def solve
   end
