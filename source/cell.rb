badb66dceebfae772da5e7065b241eca3a32b8d9
Ashley Baldwin-Hunter <ashley.baldwinhunter@gmail.com>
2015-04-11 16:43:49 -0400

    add methods for accessing cell row, col and block

--- source/cell.rb
+++ source/cell.rb
@@ -1,5 +1,32 @@
 class Cell
+  attr_accessor :index, :contents, :determined, :possibilities
+  attr_reader :board, :col, :row, :block
 
+  def initialize(args)
+    @index = args[:index]
+    @contents = args[:contents] || nil
+    @determined
+    @possibilities
+    @board = args[:board]
+    @x_dim = board.x_dim || 9
+    @block
+    get_block
+  end
 
+  def row
+    index % x_dim
+  end
+
+  def col
+    index % x_dim
+  end
+
+  def get_block
+    board.blocks.each_with_index do |block, i|
+      if block.include?(self.index)
+        self.block = i
+      end
+    end
+  end
 
 end
--- source/sudoku.rb
+++ source/sudoku.rb
@@ -2,11 +2,13 @@ require_relative 'cell'
 
 class Sudoku
   attr_accessor :board, :possibilities
+  attr_reader :x_dim
 
   def initialize(board_string)
     @board = board_string.split("")
     @possibilities = Hash.new
     initialize_possibilities!
+    @x_dim = 9
   end
 
   def initialize_possibilities!
@@ -53,6 +55,29 @@ class Sudoku
   # Returns a nicely formatted string representing the current state of the board
   def to_s
   end
+
+  def print_board
+
+
+
+  end
+
+  def clear_screen!
+    print "\e[2J"
+  end
+
+
+
 end
 
+  # 0   1   2   3  4  5   6   7   8
+  # 9  10  11  12 13 14  15  16  17
+  # 18 19  20  21 22 23  24  25  26
 
+  # 27 28  29  30 31 32  33  34  35
+  # 36 37  38  39 40 41  42  43  44
+  # 45 46  47  48 49 50  51  52  53
+
+  # 54 55  56  57 58 59  60  61  62
+  # 63 64  65  66 67 68  69  70  71
+  # 72 73  74  75 76 77  78  79  80
